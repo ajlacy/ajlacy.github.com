95e1685f5561b6070a7829b4fd712dfe9756a7c5
Ashley Lacy <ashleyjlacy@gmail.com>
2015-07-24 01:50:35 +0000

    added more matchy

--- projects/matchy/part2.js
+++ projects/matchy/part2.js
@@ -8,10 +8,46 @@ console.log(profileAnimal);
 
 //Loop through the properties of the animal, console.log the property name and if the property value is a string, also print that out. If the value is not a string, then just put a placeholder value of 'click here for data' (not actually a link)
 //'species: dog, tagline: who let the dogs out?, noises: click here for more data, relationships: click here for more data' etc
-for(var i = 0; i === animal.species; i++) {
-     
+for(var key in profileAnimal) {
+     if(Array.isArray(profileAnimal[key])) {
+         console.log(key + ": " + profileAnimal[key]);
+     } else if (typeof profileAnimal[key] === "string"){
+         console.log(key + ": " + profileAnimal[key]);
+         
+     }else {
+         console.log(key +  ": " + "Click here for data");
+     }
 }
 
 
 //Adjust the previous loop so that it will loop through the noises array and print it out like this:
 //'species: dog, tagline: who let the dogs out?, noises: woof bark slurp , relationships: click here for more data'
+
+//Access the relationships object
+
+//Log all the friends of the animal
+
+//Now, log all the matches
+
+//How woold you delete a friend from your friendslist? Implement it!
+
+var relationships = {animals: matches
+}
+
+
+function each(collection, action) {
+ if (Array.isArray(collection)) {
+  for (var i = 0; i < collection.length; i++){
+   action(collection[i]);
+  }
+ } else {
+   for (var key in collection) {
+     action(collection[key]);
+   }
+ }
+}
+console.log(profileAnimal.relationships.friends);
+console.log(profileAnimal.relatioships.matches);
+
+profileAnimal.relationships.friends.splice(0, 1);
+console.log(profileAnimal.relationships.friends);
