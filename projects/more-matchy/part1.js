8e13763979acfb3648f6fb4eaf7427d558de1b35
Ashley Lacy <ashleyjlacy@gmail.com>
2015-07-29 01:38:16 +0000

    on the third helper function

--- projects/more-matchy/part1.js
+++ projects/more-matchy/part1.js
@@ -3,16 +3,54 @@
 
 
 $.getJSON("data/animals.json", function(data) {
-        
 
-        var animal;
 
-        //example input: { species: 'duck', tagline: 'Afflac', noises: ['quack', 'honk', 'sneeze', 'growl'] }
-        //example output: "species tagline noises"
+    var animal;
 
-        function objKeyPrinter(obj) {
-            return Object.keys(obj).join(" ");
-            
-        }    
-        console.log(objKeyPrinter(data[0]));
+    //example input: { species: 'duck', tagline: 'Afflac', noises: ['quack', 'honk', 'sneeze', 'growl'] }
+    //example output: "species tagline noises"
+
+    function objKeyPrinter(obj) {
+        return Object.keys(obj).join(" "); //.keys returns an array
+
+    }
+    console.log(objKeyPrinter(data[0]));
+    //objValuePrinter loops through all the properties in a given object and returns a string of all the values that are strings.
+    //example input: { species: 'duck', tagline: 'Afflac', noises: ['quack', 'honk', 'sneeze', 'growl'] }
+    //example output: "duck Afflac"
+
+    function objValuePrinter(obj) {
+        var strings = [];
+        for (var key in obj) {
+            if (typeof(obj[key]) === "string") {
+                strings.push(obj[key]);
+            }
+        }
+        return strings.join(" "); //.join joins all element of an array into a string
+    }
+console.log(objValuePrinter(data[2]));    
+
+//arrValuePrinter takes a given array and returns the values as a string
+//example input: ['quack', 'honk', 'sneeze', 'growl']
+//example output: "quack honk sneeze growl"
+
+var joined = [];
+
+function arrValuePrinter(collection) {
+   if (Array.isArray(collection)) {
+       joined = joined.push(collection);
+   } else {
+       for (var key in collection) {
+           if (Array.isArray(collection[key])) {
+            return collection[key];
+       }
+   }
+} 
+    return joined.join(" ");
+}
+console.log(arrValuePrinter(data[0]));  
+  
 });
+
+
+
