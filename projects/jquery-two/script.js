055c9453bb26989b3b9a48ca88288cf931690ced
Ashley Lacy <ashleyjlacy@gmail.com>
2015-08-06 00:54:54 +0000

    updated avengers

--- projects/jquery-two/script.js
+++ projects/jquery-two/script.js
@@ -1,33 +1,48 @@
-$(function() {
+$(function () {
+    var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
 
-  var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
-
- $('#avengers').append(createUnorderedList(avengers)); 
- 
-  
-  // Code goes here!
-  function createUnorderedList(list) {
-    list = list.sort();
-      var $ul = ('ul/');
-      list.forEach(function(item) {
-       var className = item.toLowerCase().split(" ").join("-")
-        $('<li/>')
-          .addClass(className)
-          .text(item)
-          .appendTo($ul);
-
-    });
-    $('#avengers').append($ul)
-    console.log($ul);
-  }
-  
-  function removeAvenger(avenger) {
-    var className = avenger.toLowerCase().split(".").join("-")
-    $("." + className).remove();
-    //console.log(className);
-  }
-  
-  function sortListAlpha($ul) {
+    $('#avengers')
+        .append(createUnorderedList(avengers, 'list-avengers', 'avenger'));
     
-  }
+    // Code goes here!
+    function createUnorderedList(array, listId, className) {
+        // list = list.sort();
+        var $ul = $('<ul/>')
+            .attr('id', listId);
+        array.forEach(function (item) {
+            // given Iron Man => iron-man //
+            var id = item.toLowerCase().split(" ").join("-");
+            $('<li/>')
+                .attr('id', id)
+                .addClass(className)
+                .text(item)
+                .appendTo($ul);
+        });
+        return $ul;
+    }
+
+    function removeAvenger(avenger) {
+        var className = avenger.toLowerCase().split(" ").join("-")
+        $("." + className).remove();
+    }
+    
+    // removeAvenger('Iron Man');
+    // removeAvenger('Wasp');
+    
+    function getData(className) {
+        var output = [];
+        var $listItems = $(className);
+        console.log($listItems);
+        $listItems.each(function (key) {
+            output.push($listItems[key].innerHTML);
+        });
+        return output;
+    }
+    var data = getData($('.avenger')).sort();
+    $('#avengers').empty();
+    var $ul = createUnorderedList(data);
+    $('#avengers').append($ul);
+    
+    
+    
 });
