681f2144910261bc1142459131bdc07f1e7b0953
Ashley Lacy <ashleyjlacy@gmail.com>
2015-08-06 00:27:13 +0000

    added to avengers

--- projects/jquery-two/script.js
+++ projects/jquery-two/script.js
@@ -1,19 +1,33 @@
-$(function(){
-  
-  var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
-  
-  prepareView(avengers);
+$(function() {
 
+  var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
+
+ $('#avengers').append(createUnorderedList(avengers)); 
+ 
+  
   // Code goes here!
-    function prepareView(list) {
+  function createUnorderedList(list) {
+    list = list.sort();
       var $ul = ('ul/');
-      list.forEach(function (item){
-        ('<li/>')
-        .addClass(item.toLowerCase().split(" ").join("-"))
-        .text(item)
-        .appendTo($ul);
-        
-      });
-      console.log($ul);
-    }
+      list.forEach(function(item) {
+       var className = item.toLowerCase().split(" ").join("-")
+        $('<li/>')
+          .addClass(className)
+          .text(item)
+          .appendTo($ul);
+
+    });
+    $('#avengers').append($ul)
+    console.log($ul);
+  }
+  
+  function removeAvenger(avenger) {
+    var className = avenger.toLowerCase().split(".").join("-")
+    $("." + className).remove();
+    //console.log(className);
+  }
+  
+  function sortListAlpha($ul) {
+    
+  }
 });
